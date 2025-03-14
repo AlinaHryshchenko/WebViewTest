702e96983efe02a98b8ebca7e881915e446f5441
Alina Hryshchenko <a.hryshchenko@4.partners>
2025-03-14 15:37:04 +0000

    first commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Alina Hryshchenko on 14/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
